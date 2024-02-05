8bde724a28e9cabd4cd4707b1a459ed6c089bbbc
Kajal <kajal1204kuk@gmail.com>
2024-02-05 12:23:05 +0530

    demo

--- src/Demo.js
+++ src/Demo.js
@@ -1,69 +1,47 @@
-import { StyleSheet,ScrollView,Text, View } from 'react-native'
-import React from 'react'
-import { black, white } from '../Practis0cial/utils/Color'
+import React from 'react';
+import { ScrollView, View, Text, StyleSheet } from 'react-native';
+import { Theme_Color1 } from '../Practis0cial/utils/Color';
 
 const Demo = () => {
-    const user = ["Box1", "Box2", "Box3", "Box3"]
-    return (
-        <ScrollView style={styles.container}>
-            {/* firstway to do task */}
-            <Text style={styles.commonHeading}>firstway to do task</Text>
+  const items = ['Aadharcard', 'Pancard', 'College Id', 'Driving Licene',];
 
-            <View style={styles.box}>
-                {user.map((item) =>
-                    <Text style={styles.grid}>{item}</Text>
-                )}
-            </View>
+  const firstFun = () => {
+    const itemViews = [];
+    for (let i = 0; i < items.length; i++) {
+      itemViews.push(
+        <View style={styles.item}>
+          <Text>{items[i]}</Text>
+        </View>
+      );
+    }
+    return itemViews;
+  };
 
+  return (
+    <View>
+    <ScrollView horizontal style={styles.scrollView}>
+      {firstFun()}
+    </ScrollView>
 
-            {/* second way to do task */}
-            <Text style={styles.commonHeading}>second way to do task</Text>
-
-            <View style={styles.box}>
-                <Text style={styles.grid}>Box 1</Text>
-                <Text style={styles.grid}>Box 2</Text>
-                <Text style={styles.grid}>Box 3</Text>
-                <Text style={styles.grid}>Box 4</Text>
-                <Text style={styles.grid}>Box 5</Text>
-                <Text style={styles.grid}>Box 6</Text>
-                <Text style={styles.grid}>Box 7</Text>
-                <Text style={styles.grid}>Box 8</Text>
-                <Text style={styles.grid}>Box 9</Text>
-                <Text style={styles.grid}>Box 10</Text>
-            </View>
-
-        </ScrollView>
-    )
-}
-
-export default Demo
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: white
-    },
-    box: {
-        flexDirection: 'row',
-        flex: 1,
-        flexWrap: "wrap",
-        alignItems: 'center',
-        marginHorizontal: 20,
-    },
-    grid: {
-        margin: 2,
-        backgroundColor: "blue",
-        textAlign: "center",
-        textAlignVertical: 'center',
-        width: 100,
-        height: 100,
-        borderRadius: 10
-    },
-    commonHeading: {
-        fontSize: 20,
-        color: black,
-        marginTop: 10,
-        marginLeft: 10,
-        textDecorationLine: 'underline'
-    }
-})
+  scrollView: {
+    flexDirection: 'row',
+  },
+  item: {
+    padding: 16,
+    margin: 8,
+    backgroundColor: 'purple',
+    borderRadius: 8,
+    width:200,
+    height:200,
+    justifyContent:'center',
+    alignItems:"center"
+  },
+
+});
+
+export default Demo;
